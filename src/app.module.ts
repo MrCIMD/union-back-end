395ae0368ed3858b96cbd6f7c89f014261a2d119
Caleb Isaac Mora Díaz <calexico659@gmail.com>
2021-05-13 19:13:01 -0500

    Create config module and read variables .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// Service
+import { ConfigService } from "./common/config/config.service";
+// Config keys
+import { Configuration } from "./common/config/config.keys";
+// Modules
+import { ConfigModule } from "./common/config/config.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  static PORT: number | string;
+  static HTTP: string;
+  static HOST: string;
+
+  constructor(private readonly _config: ConfigService) {
+    AppModule.PORT = this._config.get(Configuration.APP_PORT);
+    AppModule.HTTP = this._config.get(Configuration.APP_HTTP_PROTOCOL);
+    AppModule.HOST = this._config.get(Configuration.APP_HOST);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('Gob App')
+    .setDescription('Backend for Union')
+    .setVersion('1.0')
+    .addTag('Endpoints')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(AppModule.PORT);
+
+  console.log(`[ Menu It Api Documentation ] host run in ${AppModule.HTTP}://${AppModule.HOST}:${AppModule.PORT}/docs`)
+  console.log(`[ Menu It ] host run in ${AppModule.HTTP}://${AppModule.HOST}:${AppModule.PORT}/api/`)
 }
 bootstrap();
